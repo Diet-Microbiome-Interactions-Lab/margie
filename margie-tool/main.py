744a237b15cba0a65abfcb14d4486c1e035d01ed
ddeemer <ddeemer@purdue.edu>
2025-10-03 17:25:34 -0400

    skeleton for example.smk rules and sqlite dependency

--- margie-tool/main.py
+++ margie-tool/main.py
@@ -13,7 +13,6 @@ import yaml
 
 from bioinformatics_tools.FileClasses.Fasta import Fasta
 
-
 def get_fasta_files(inputs):
     """Expand directories and collect all FASTA files."""
     fasta_files = []
@@ -118,8 +117,9 @@ def main():
     output_dir = workflow_config_defaults.get('output_dir', 'output')
     for sample in samples:
         # if 'make_db' in rules_to_run:
-        if True:
-            targets.append(f"{output_dir}/{sample}.db")
+        if True:  #TODO: Make this dynanmic based on the CLI
+            # targets.append(f"{output_dir}/{sample}.db")
+            targets.append(f"{output_dir}/annotations/agg/{sample}.gff")
     print(f'\nStep 5: Target files to be generated: {targets}')
 
     # Create sample to file mapping
@@ -160,6 +160,8 @@ def main():
 
     # ---------------------- Step 8: Run the actual command ---------------------- #
     result = subprocess.run(cmd, capture_output=True, text=True, cwd=Path(__file__).parent)
+    print(f'Snakemake stdout:\n{result.stdout}')
+    print(f'Snakemake stderr:\n{result.stderr}')
 
     if result.returncode == 0:
         print("Snakemake pipeline completed successfully!")
